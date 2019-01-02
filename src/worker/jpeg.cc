a5b1204cf79d774a1aa363d8840762d067efaec4
Kuntal Majumder <hellozee@disroot.org>
2019-01-02 20:34:41 +0530

    Added JPEG workers to save and load jpeg images

---
+++ src/worker/jpeg.cc
@@ -0,0 +1,114 @@
+#include "jpeg.hh"
+
+FIIO::jpeg::worker::worker(const std::string &filename)
+{
+    auto decompress_dt = [] ( ::jpeg_decompress_struct *ds){
+        ::jpeg_destroy_decompress(ds);
+    };
+
+    std::unique_ptr<::jpeg_decompress_struct, decltype(decompress_dt)> cinfo( 
+                                    new ::jpeg_decompress_struct, decompress_dt );
+
+    _error_manager = std::make_shared<::jpeg_error_mgr>();
+
+    auto file_dt = [] (FILE *file) {
+        fclose(file);
+    };
+
+    std::unique_ptr<FILE, decltype(file_dt)> infile(
+                                        fopen(filename.c_str(), "rb"), file_dt);
+
+    if(infile.get() == NULL) {
+        throw std::runtime_error("Can't find the file sorry");
+    }
+
+    cinfo->err = ::jpeg_std_error(_error_manager.get());
+    _error_manager->error_exit = []( ::j_common_ptr cinfo){
+        char error_message[JMSG_LENGTH_MAX];
+        (*(cinfo->err->format_message))(cinfo, error_message);
+        throw std::runtime_error(error_message);
+    };
+
+    ::jpeg_create_decompress(cinfo.get());
+    ::jpeg_stdio_src(cinfo.get(), infile.get());
+    auto temp = ::jpeg_read_header(cinfo.get(), true);
+    if(temp != 1){
+        throw std::runtime_error("This is not JAYPEG.");
+    }
+
+    ::jpeg_start_decompress(cinfo.get());
+
+    _width = cinfo->output_width;
+    _height = cinfo->output_height;
+    _pixel_size = cinfo->output_components;
+    _colorspace = cinfo->out_color_space;
+
+    size_t row_stride = _width * _pixel_size;
+    _pixels.clear();
+    _pixels.reserve(_height);
+
+    while(cinfo->output_scanline < _height){
+        std::vector<uint8_t> row_vector(row_stride);
+        uint8_t *data = row_vector.data();
+        ::jpeg_read_scanlines(cinfo.get(), &data, 1);
+        _pixels.push_back(row_vector);
+    }
+
+    ::jpeg_finish_decompress(cinfo.get());
+}
+
+FIIO::jpeg::worker::worker(const FIIO::jpeg::worker& rhs)
+{
+    _error_manager = rhs._error_manager;
+    _pixels = rhs._pixels;
+    _width = rhs._width;
+    _height = rhs._height;
+    _pixel_size = rhs._pixel_size;
+    _colorspace = rhs._colorspace;
+}
+
+void
+FIIO::jpeg::worker::save(const std::string &filename, int quality)
+{
+    if(quality < 0)
+        quality = 0;
+    if(quality > 100)
+        quality = 100;
+
+    auto file_dt = [] (FILE *file) {
+        fclose(file);
+    };
+
+    std::unique_ptr<FILE, decltype(file_dt)> outfile(
+                                        fopen(filename.c_str(), "rb"), file_dt);
+
+    if(outfile == NULL){
+        throw std::runtime_error("Could not open " + filename);
+    }
+
+    auto compress_dt = []( ::jpeg_compress_struct *cs){
+        ::jpeg_destroy_compress(cs);
+    };
+
+    std::unique_ptr<::jpeg_compress_struct, decltype(compress_dt)>cinfo(
+                                        new ::jpeg_compress_struct, compress_dt);
+
+    ::jpeg_create_compress(cinfo.get());
+    ::jpeg_stdio_dest(cinfo.get(), outfile.get());
+    cinfo->image_width = _width;
+    cinfo->image_height = _height;
+    cinfo->input_components = _pixel_size;
+    cinfo->in_color_space = static_cast<::J_COLOR_SPACE>(_colorspace);
+    cinfo->err = ::jpeg_std_error(_error_manager.get());
+    ::jpeg_set_defaults(cinfo.get());
+    ::jpeg_set_quality(cinfo.get(), quality, true);
+    ::jpeg_start_compress(cinfo.get(), true);
+
+    for(auto const& row_vector : _pixels){
+        ::JSAMPROW buffer[1];
+        buffer[0] = const_cast<::JSAMPROW>(row_vector.data());
+        ::jpeg_write_scanlines(cinfo.get(), buffer, 1);
+    }
+
+    ::jpeg_finish_compress(cinfo.get());
+}
---
+++ src/worker/jpeg.hh
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+#include <jpeglib.h>
+#include <memory>
+#include <vector>
+
+namespace FIIO::jpeg{
+    class worker{
+        public:
+            explicit worker(const std::string &filename);
+            worker(const worker& rhs);
+
+            void save(const std::string &filename, int quality=90);
+
+            inline size_t width() { return _width; }
+            inline size_t height() { return _height; }
+            inline size_t pixel_size() { return _pixel_size; }
+            inline int colorspace() { return _colorspace; }
+
+        private:
+            int _colorspace;
+            size_t _width, _height, _pixel_size;
+            std::shared_ptr<::jpeg_error_mgr> _error_manager;
+            std::vector<std::vector<uint8_t>> _pixels;
+    };
+}
