f6f90d3bc7971c3dec66f0ef6b52a231b4db4317
Kuntal Majumder <hellozee@disroot.org>
2019-01-04 13:36:30 +0530

    woot, initial picture of the api

--- src/FImageIO.cc
+++ src/FImageIO.cc
@@ -1,1 +1,23 @@
 #include <FImageIO/FImageIO>
+#include "worker/jpeg.hh"
+
+#include <experimental/filesystem>
+
+namespace fs = std::experimental::filesystem;
+
+fiio::image::image(std::string fname):
+    _filename(fname)
+{
+    fs::path p(fname); 
+    auto extension = p.extension().u8string();
+
+    if(extension == ""){
+        throw std::runtime_error("I am not sure about the format, please \
+                                    give the file an extension");
+    }
+
+    if(extension == "jpg" || extension == "jpeg" || extension == "jpe" ||
+            extension == "jif" || extension == "jfi" || extension == "jfif") {
+       _image_data = std::make_shared<fiio::jpeg::worker>(fname); 
+    }
+}
