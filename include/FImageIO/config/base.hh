c2b1c11c35d31e2e79c7e0351017b5854dfbd4b9
Kuntal Majumder <hellozee@disroot.org>
2019-01-05 14:35:38 +0530

    Move the image_formats to a more apt header

--- include/FImageIO/config/base.hh
+++ include/FImageIO/config/base.hh
@@ -2,10 +2,20 @@
 
 #include <string>
 
+namespace fiio{
+    enum class image_format{
+        jpeg,
+        png
+    };
+}
+
 namespace fiio::config{
     class base{
     public:
+        explicit base(fiio::image_format format);
         virtual void save(std::string fname) = 0;
         virtual void load(std::string fname) = 0;
+    private:
+        fiio::image_format _format;
     };
 }
