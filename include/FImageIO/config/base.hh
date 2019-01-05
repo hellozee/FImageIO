d4bc719451b081c4f1484b84cf4d5028795a146b
Kuntal Majumder <hellozee@disroot.org>
2019-01-05 15:18:01 +0530

    added a format getter to config base

--- include/FImageIO/config/base.hh
+++ include/FImageIO/config/base.hh
@@ -13,8 +13,12 @@ namespace fiio::config{
     class base{
     public:
         explicit base(fiio::image_format format);
+
         virtual void save(std::string fname) = 0;
         virtual void load(std::string fname) = 0;
+
+        inline fiio::image_format format() { return _format; }
+
     private:
         fiio::image_format _format;
     };
