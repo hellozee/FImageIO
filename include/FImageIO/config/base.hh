3538cc21818727215db09afae8eb4c8fe2b14590
Kuntal Majumder <hellozee@disroot.org>
2019-01-06 12:34:10 +0530

    Updated base class for including filename by default

--- include/FImageIO/config/base.hh
+++ include/FImageIO/config/base.hh
@@ -13,13 +13,16 @@ namespace fiio::config{
     class base{
     public:
         explicit base(fiio::image_format format);
+        base(fiio::image_format format, std::string fname);
 
         virtual void save(std::string fname) = 0;
         virtual void load(std::string fname) = 0;
 
         inline fiio::image_format format() { return _format; }
+        inline std::string filename() { return _filename; }
 
     private:
         fiio::image_format _format;
+        std::string _filename = "";
     };
 }
