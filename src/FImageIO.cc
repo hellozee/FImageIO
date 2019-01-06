5016a57c4a03b08eda36c8955e38d098eefa87a5
Kuntal Majumder <hellozee@disroot.org>
2019-01-06 19:01:00 +0530

    dynamic casting seems more reliable with modern c++

--- src/FImageIO.cc
+++ src/FImageIO.cc
@@ -18,21 +18,32 @@ fiio::image::image(std::string fname):
 
     if(extension == "jpg" || extension == "jpeg" || extension == "jpe" ||
             extension == "jif" || extension == "jfi" || extension == "jfif") {
-       _image_data = std::make_shared<fiio::jpeg::worker>(fname); 
+       _image_data = std::make_shared<fiio::worker::jpeg>(fname); 
     }
 }
 
 void
 fiio::image::save(const std::string &fname,
-        std::unique_ptr<fiio::config::base> config)
+        std::shared_ptr<fiio::config::base> conf)
 {
-    switch (config->format()){
+    switch (conf->format()){
         case fiio::image_format::jpeg:{
+            // casts to fiio::config::jpeg and calls the respective function
+            auto jpeg_conf = std::dynamic_pointer_cast<fiio::config::jpeg>(conf);    
+            save_jpeg_config(fname, jpeg_conf);
             break;
         }
 
         case fiio::image_format::png:{
+            // casts to fiio::config::png and calls the respective function
             break;
         }
     }
+}
+
+void 
+fiio::image::save_jpeg_config(const std::string &fname,
+        std::shared_ptr<fiio::config::jpeg> conf)
+{
+    // haven't started working on serialization
 }
